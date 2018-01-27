faf773c633e7c104e07ecac3eb22aa08a596dd80
marta83 <prettykiller777@gmail.com>
2018-01-27 23:15:33 +0100

    Check username and pass

--- src/Trovit/Bundle/UserBundle/Controller/UserController.php
+++ src/Trovit/Bundle/UserBundle/Controller/UserController.php
@@ -4,15 +4,31 @@ namespace Trovit\Bundle\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class UserController extends Controller
 {
     public function checkLoginAction(Request $request)
     {
+        $redis = $this->container->get('snc_redis.default');
+        $val = $redis->set('orugrita','1234');
+
         $username = $request->query->get('username');
         $pass = $request->query->get('pass');
 
-        return new Response();
+        $saved_pass = $redis->get($username);
+
+        if(!isset($saved_pass))
+        {
+          return new JsonResponse(array('success' => false, 'message' => "User does not exist."));
+        }
+
+        if($pass != $saved_pass )
+        {
+          return new JsonResponse(array('success' => false, 'message' => "User and Password do not match."));
+        }
+
+
+        return new JsonResponse(array('success' => true, 'message' => "User and Password match."));
     }
 }
--- tests/TrovitUserBundle/Controller/UserFunctionalTest.php
+++ tests/TrovitUserBundle/Controller/UserFunctionalTest.php
@@ -20,4 +20,32 @@ class UserFunctionalTest extends WebTestCase
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
     }
 
+    public function testUserMustExist()
+    {
+        $this->client->request('GET','/check-login?username=nonexisteduser&pass=54FgtWrs68');
+
+        $data = $this->client->getResponse()->getContent();
+        $this->assertJsonStringEqualsJsonString(json_encode(array('success' => false, 'message' => "User does not exist." )), $data);
+
+    }
+
+    public function testUserAndPasswordMustMatch()
+    {
+        $this->client->request('GET','/check-login?username=orugrita&pass=tWrs68');
+
+        $data = $this->client->getResponse()->getContent();
+        $this->assertJsonStringEqualsJsonString(json_encode(array('success' => false, 'message' => "User and Password do not match." )), $data);
+
+    }
+
+    public function testUserAndPasswordMatch()
+    {
+        $this->client->request('GET','/check-login?username=orugrita&pass=1234');
+
+        $data = $this->client->getResponse()->getContent();
+        $this->assertJsonStringEqualsJsonString(json_encode(array('success' => true, 'message' => "User and Password match." )), $data);
+
+    }
+
+
 }
